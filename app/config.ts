3a03535ca700c66d32134a75ea089caf17d6653e
Brandon Echols <brandonechols04@gmail.com>
2025-05-04 00:02:13 -0700

    Initial commit

--- app/config.ts
+++ app/config.ts
@@ -1,7 +1,7 @@
 export const metaData = {
-  baseUrl: "https://nextfolio-template.vercel.app/",
-  title: "Nextfolio",
-  name: "Sirius",
+  baseUrl: "https://brandonechols.vercel.app/",
+  title: "Brandon Echols",
+  name: "Brandon Echols",
   ogImage: "/opengraph-image.png",
   description:
     "A clean, fast, and lightweight portfolio template built with Next.js, Vercel, and Tailwind CSS for optimal performance.",
