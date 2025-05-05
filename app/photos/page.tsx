1af4ad3af6e1b88201f5d871c240e1e4d15bfcd1
Brandon Echols <brandonechols04@gmail.com>
2025-05-04 21:53:17 -0700

    Added photos

--- app/photos/page.tsx
+++ app/photos/page.tsx
@@ -12,60 +12,21 @@ export default function Photos() {
     <section>
       <h1 className="mb-8 text-2xl font-medium">Photos from the Wild Mind</h1>
       <ImageGrid
-        columns={3}
-        images={[
-          {
-            src: "/photos/photo1.jpg",
-            alt: "Roman columns",
-            href: "https://unsplash.com/photos/people-walking-near-building-during-daytime-dFLBDQQeffU?utm_content=creditShareLink&utm_medium=referral&utm_source=unsplash",
-          },
-          {
-            src: "/photos/photo2.jpg",
-            alt: "Big Ben",
-            href: "https://unsplash.com/photos/big-ben-london-MdJq0zFUwrw?utm_content=creditShareLink&utm_medium=referral&utm_source=unsplash",
-          },
-          {
-            src: "/photos/photo3.jpg",
-            alt: "Sacré-Cœur Basilica",
-            href: "https://unsplash.com/photos/a-view-of-the-inside-of-a-building-through-a-circular-window-Tp-3hrx88J4",
-          },
-          {
-            src: "/photos/photo4.jpg",
-            alt: "Eiffel Tower",
-            href: "https://unsplash.com/photos/the-eiffel-tower-towering-over-the-city-of-paris-OgPuPvPsHLM?utm_content=creditShareLink&utm_medium=referral&utm_source=unsplash",
-          },
-          {
-            src: "/photos/photo5.jpg",
-            alt: "Taj Mahal",
-            href: "https://unsplash.com/photos/taj-mahal-india-IPlPkWPJ2fo",
-          },
-          {
-            src: "/photos/photo6.jpg",
-            alt: "Colosseum",
-            href: "https://unsplash.com/photos/brown-concrete-building-under-blue-sky-during-daytime-3cyBR1rIJmA?utm_content=creditShareLink&utm_medium=referral&utm_source=unsplash",
-          },
-        ]}
-      />
-
-      <ImageGrid
         columns={2}
         images={[
-          { src: "/photos/photo1.jpg", alt: "Roman columns" },
-          { src: "/photos/photo2.jpg", alt: "Big Ben" },
-          { src: "/photos/photo3.jpg", alt: "Sacré-Cœur Basilica" },
-          { src: "/photos/photo4.jpg", alt: "Eiffel Tower" },
-        ]}
-      />
-
-      <ImageGrid
-        columns={4}
-        images={[
-          { src: "/photos/photo1.jpg", alt: "Roman columns" },
-          { src: "/photos/photo2.jpg", alt: "Big Ben" },
-          { src: "/photos/photo3.jpg", alt: "Sacré-Cœur Basilica" },
-          { src: "/photos/photo4.jpg", alt: "Eiffel Tower" },
-          { src: "/photos/photo5.jpg", alt: "Taj Mahal" },
-          { src: "/photos/photo6.jpg", alt: "Colosseum" },
+          { src: "/photos/photo13.jpg", alt: "Shark-fin shaped rock jetting out of ocean" },
+          { src: "/photos/photo12.jpg", alt: "Color-pop mushroom close up" },
+          { src: "/photos/photo11.jpg", alt: "Mushroom close up" },
+          { src: "/photos/photo10.jpg", alt: "Mini forest path" },
+          { src: "/photos/photo9.jpg", alt: "Layered foliage, trees, and sky" },
+          { src: "/photos/photo8.jpg", alt: "Teenager biking on forest road" },
+          { src: "/photos/photo7.jpg", alt: "Sunset and tree silhouette" },
+          { src: "/photos/photo6.jpg", alt: "Medford Oregon Temple" },
+          { src: "/photos/photo5.jpg", alt: "Cloudy sky" },
+          { src: "/photos/photo4.jpg", alt: "Tree skyline" },
+          { src: "/photos/photo3.jpg", alt: "Milky way galaxy" },
+          { src: "/photos/photo2.jpg", alt: "Brown eyed susan" },
+          { src: "/photos/photo1.jpg", alt: "Yellow poppy" },
         ]}
       />
     </section>
