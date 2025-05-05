ad9d9eb19567c5f0de5db55a18c113eebcbb054a
Brandon Echols <brandonechols04@gmail.com>
2025-05-05 12:39:01 -0700

    Added poetry placeholder

--- app/projects/project-data.tsx
+++ app/projects/project-data.tsx
@@ -7,13 +7,19 @@ export interface Project {
 
 export const projects: Project[] = [
   {
+    title: "Poetry Corpus",
+    year: 2025,
+    description: "All my poetry",
+    url: "https://hahathisdoesntexist.yet",
+  },
+  {
     title: "Gospel Encyclopedia",
     year: 2025,
-    description: "Work-in-progress encyclopedia of the Gospel",
+    description: "WIP encyclopedia",
     url: "https://docs.google.com/document/d/18vfrlADPLz5144r5mVEEEeZdcxdNeEk2/",
   },
   {
-    title: "Digital Resources & Education Paper",
+    title: "The positive impacts of digital resources on reading and...",
     year: 2025,
     description: "Research paper",
     url: "https://docs.google.com/document/d/1GqTKUCUpkw7Y5pxkoZif_ji5E_lHSqi6/",
