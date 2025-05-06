b2b541aad95ae6b249d4513de1b6a634f80c9705
Brandon Echols <brandonechols04@gmail.com>
2025-05-05 19:12:57 -0700

    Changed main page

--- app/page.tsx
+++ app/page.tsx
@@ -9,13 +9,16 @@ export default function Page() {
       </h1>
       <div className="prose prose-neutral dark:prose-invert">
          <p>
-           Hi, I'm Brandon Echols and this is <i>The Wild Mind</i>. That is, you'll find a bunch of sentiments from <i>my</i> <b>wild</b> mind here.
+           Hi, I'm Brandon Echols and this is <i>The Wild Mind</i>. That is, you'll find a bunch of sentiments from my wild mind right here. Having an interest in many subsets of life, you'll find a variety of things covered—writing, music, religion, D&D, genealogy, photography, and more!
          </p>
          <p>
            At the top of the page, you'll see three tabs. The <b>Blog</b> tab is where you'll find all of my random thoughts and musings.
          </p>
          <p>
-           In <b>Projects</b>, you might find a variety of links—projects I'm working on, projects I've finished (however seldom that occurs), and more.
+           In <b>Projects</b>, you might find a variety of links to projects I'm working on and projects I've finished (however seldom that occurs).
+         </p>
+         <p>
+           Within <b>Collections</b>, you navigate through various collections of my poetry, music, etc.
          </p>
          <p>
            Under the <b>Photos</b> tab, you can peruse a gallery of photographs that I have captured. The newest images will appear near the top.
